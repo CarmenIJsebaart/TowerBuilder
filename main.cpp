2fb7cd360a27a4c4100c660c679ba0640c57b7c1
CarmenIJsebaart <cijsebaart@gmail.com>
2016-10-20 20:33:33 +0200

    Builds only taking y into account

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 
+void check_collision(sf::RectangleShape initial_block, int &build_block_y_position, std::vector<sf::RectangleShape> block_vector);
 void check_movement_direction(sf::RectangleShape build_block, const int window_width, const int block_size, bool &block_move_right, bool &block_move_left);
 void draw_blocks(sf::RenderWindow &window, sf::RectangleShape &build_block, std::vector<sf::RectangleShape> block_vector);
 void move_block(sf::RectangleShape &build_block, bool block_move_right, bool block_move_left);
@@ -12,9 +13,18 @@ int main()
    const int window_width = 300;
    sf::RenderWindow window(sf::VideoMode(window_width, window_height), "TowerBuilder", sf::Style::Titlebar | sf::Style::Close);
 
+   sf::RectangleShape initial_block;
+   const int initial_block_size = 50;
+   const int initial_block_x_position = (window_width - initial_block_size) / 2; //in the middle of the screen
+   const int initial_block_y_position = window_height - initial_block_size; //at the bottom of the screen
+
+   initial_block.setSize(sf::Vector2f(initial_block_size, initial_block_size));
+   initial_block.setPosition(initial_block_x_position, initial_block_y_position);
+   initial_block.setFillColor(sf::Color::Yellow);
+
    sf::RectangleShape build_block;
-   const int block_size = 50;
-   const int build_block_x_position = 0;
+   int block_size = 50;
+   int build_block_x_position = 0;
    int build_block_y_position = 20;
 
    build_block.setSize(sf::Vector2f(block_size, block_size));
@@ -38,21 +48,27 @@ int main()
         {
           case sf::Event::Closed:
             window.close();
-             break;
-           case sf::Event::KeyPressed:
-             if(sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
-             {
-                build_block.move(0, window_height - block_size - build_block_y_position);
-                block_vector.push_back(build_block);
-                build_block.setPosition(0, 20);
-             }
-             break;
-           default:
-             break;
+            break;
+          case sf::Event::KeyPressed:
+            if(sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
+            {
+               check_collision(initial_block, build_block_y_position, block_vector);
+               std::cout << build_block_y_position << std::endl;
+               build_block.setPosition(build_block.getPosition().x, build_block_y_position);
+               block_vector.push_back(build_block);
+               build_block.setPosition(0, 20);
+               build_block_y_position = 20;
+            }
+            break;
+          default:
+            break;
          }
       }
 
+
      const double update_time = 10; //milliseconds
+
+
 
      if(clock.getElapsedTime().asMilliseconds() >= update_time)
      {
@@ -61,11 +77,33 @@ int main()
      }
 
      window.clear();
+     window.draw(initial_block);
      draw_blocks(window, build_block, block_vector);
      window.display();
 
      check_movement_direction(build_block, window_width, block_size, block_move_right, block_move_left);
    }
+}
+
+void check_collision(sf::RectangleShape initial_block, int &build_block_y_position, std::vector<sf::RectangleShape> block_vector)
+{
+  if(block_vector.size() == 0)
+  {
+    do
+    {
+      build_block_y_position += 1;
+    }
+    while(build_block_y_position < initial_block.getPosition().y - 50);
+  }
+  else
+  {
+    int size = block_vector.size();
+    do
+    {
+      build_block_y_position += 1;
+    }
+    while(build_block_y_position < block_vector[size - 1].getPosition().y - 50);
+  }
 }
 
 void check_movement_direction(sf::RectangleShape build_block, const int window_width, const int block_size, bool &block_move_right, bool &block_move_left)
@@ -97,12 +135,12 @@ void draw_blocks(sf::RenderWindow &window, sf::RectangleShape &build_block, std:
 
 void move_block(sf::RectangleShape &build_block, bool block_move_right, bool block_move_left)
 {
-    if(block_move_right == true)
-    {
-      build_block.move(1, 0);
-    }
-    else if(block_move_left == true)
-    {
-      build_block.move(-1, 0);
-    }
+  if(block_move_right == true)
+  {
+    build_block.move(1, 0);
+  }
+  else if(block_move_left == true)
+  {
+    build_block.move(-1, 0);
+  }
 }
